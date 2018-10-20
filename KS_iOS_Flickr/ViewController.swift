61bf71f7117991d9a6966a820fef63b90d65bbba
Po6uHryD <32820140+Po6uHryD@users.noreply.github.com>
2018-10-20 09:51:14 +0200

    Init commit

---
+++ KS_iOS_Flickr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios.flickr
+//
+//  Created by Sergey Kiselev on 16.10.2018.
+//  Copyright © 2018 aspb.pro.home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
